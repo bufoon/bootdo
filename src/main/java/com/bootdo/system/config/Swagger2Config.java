366a07d0ad79bc1debca2608079bb126f317de53
bufoon <285395841@qq.com>
2018-06-04 21:45:20 +0800

    接口文档添加分组

--- src/main/java/com/bootdo/system/config/Swagger2Config.java
+++ src/main/java/com/bootdo/system/config/Swagger2Config.java
@@ -21,6 +21,31 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 public class Swagger2Config {
 
+    @Bean Docket createXcxAPi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("小程序API")
+                .apiInfo(xcxApiInfo())
+                .select()
+                //为当前包路径
+                .apis(RequestHandlerSelectors.basePackage("com.bootdo.xcx.api"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    //构建 api文档的详细信息函数
+    private ApiInfo xcxApiInfo() {
+        return new ApiInfoBuilder()
+                //页面标题
+                .title("书画小程序API")
+                //创建人
+                .contact(new Contact("Bufoon", "www.manteng360.com", "285395841@qq.com"))
+                //版本号
+                .version("1.0")
+                //描述
+                .description("书画小程序API接口")
+                .build();
+    }
+
     @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
